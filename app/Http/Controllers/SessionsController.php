6d82f657d4fea8b60e20c412ae2f489a7d6ec3e3
samliu <sam.liuxiaohui@foxmail.com>
2021-11-21 03:08:22 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,10 +27,17 @@ class SessionsController extends Controller
         ]);
 
         if(Auth::attempt($credentials,$request->has('remember'))){
-            //登录成功后的相关操作
-            session()->flash('success','欢迎回来！');
-            $fallback = route('users.show',Auth::user());
-            return redirect()->intended($fallback);
+            //登录成功后且已激活进行的相关操作
+            if(Auth::user()->activated){
+                session()->flash('success','欢迎回来！');
+                $fallback = route('users.show',Auth::user());
+                return redirect()->intended($fallback);
+            }else{
+                //没有激活则注销登录，返回首页
+                Auth::logout();
+                session()-flash('warning','你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect('/');
+            }
         }else{
             //登录失败后的相关操作
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
